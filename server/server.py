6f2badb0fb401af512f94c13ff57ae71964fff7f
Abdul Rahim <k213951@nu.edu.pk>
2024-10-29 17:25:37 +0500

    beginning with client side config

--- server/server.py
+++ server/server.py
@@ -1,1 +1,20 @@
 # all flask code will be here
+from flask import Flask, jsonify
+
+app = Flask(__name__) #creates a flash instance
+
+@app.route("/", methods=['Get'])
+def main_page():
+    return jsonify({
+        "message":"Main Page"
+    })
+
+@app.route("/api/home", methods=["GET"])
+def return_home():
+    return jsonify({
+        'message':"hello world"
+    })
+
+#to run our app
+if __name__ == "__main__":
+    app.run(debug=True) #dev mode ; remove when deploying to production
